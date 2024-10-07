668fd929ffcfafd084c7eb43bc5563320060758a
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-10-07 18:02:17 +0700

    Updated properties, added deps...

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     }
     @GetMapping("/tpt")
     public String getTPT() {
-        return "Thuy Phuoc Thinh";
+        return "Thuy Phuoc Thinh TPT TPT";
     }
 }
 
@@ -22,4 +22,32 @@ public class HelloController {
  * Spring Boot
  * ....
  * Hoc Spring => hoc he sinh thai Spring de viet web
+ * Maven => build a java spring project
+ * Webpack => build a reactjs/vue project
+ * Vite => build a react/vue project
+ * node => build an express server
+ * ....
+ * Build: source => target
+ * POM.xml => metadata about the project configuration => which depedencies, how to build
+ * - Compile
+ * - Build
+ * - Run
+ */
+
+/**
+ * Spring Architecture
+ * 1. Inversion of Control (theory) => Dependency Injection (a way to implement IoC)
+ * - Reuse instances of classes => instead of creating multiple similar instances
+ * - Improve performance
+ * - Improve memory allocation
+ * 2. How Spring container manages Beans
+ * - Components: a web is composed of many components, each has its own function
+ * + Components are managed by Spring container
+ * - Component scan => marked by @Component ~ an instance ~ registerred to container
+ * - Component
+ * + Beans
+ * + Services
+ * + ...
+ * + Defined ourselves
+ * 3. Tightly coupled and loosely coupled
  */
